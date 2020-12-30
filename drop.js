5f4e2e064015685cc2e294c6a2e1b8bfd8e3667f
Maulik-Agrawal <71451155+Maulik-Agrawal@users.noreply.github.com>
2020-12-30 19:33:07 +0530

    Add files via upload

---
+++ drop.js
@@ -0,0 +1,28 @@
+class Drop{
+    constructor(x,y,){
+        var options={
+            restitution: 0.1,
+            friction: 0.1
+        }
+        this.x = x;
+        this.y = y;
+        this.r = 1;
+        this.body = Bodies.circle(x,y,3,options);
+        World.add(world,this.body);
+    }
+
+    showDrop()
+    {
+         fill("blue")
+          ellipseMode(CENTER);
+           ellipse(this.body.position.x,this.body.position.y,this.radius,this.radius);
+         }
+
+         updateY()
+         {
+              if(this.body.position.y > height)
+              {
+                   Matter.Body.setPosition(this.body, {x:random(0,400), y:random(0,400)})
+                 }
+                  }
+}
